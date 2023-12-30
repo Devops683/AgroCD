9c462d61ed9e4f06a96c4a4e4166cfa0551a9d24
Devops683 <143110268+Devops683@users.noreply.github.com>
2023-12-30 18:57:25 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cd386" # Replace with your actual S3 bucket name
+    bucket = "eks386" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
